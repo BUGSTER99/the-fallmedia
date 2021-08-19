5ce5dbb07565cce2ec9e2f66a2ffbd6562f6387d
FaithL3S5 <fatihfahroji10@gmail.com>
2021-08-19 14:34:10 +0700

    first iteration, should be everything

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports={
+    env:{
+        CLOUDINARY_URL:	"https://api.cloudinary.com/v1_1/faith-l3s5/image/upload"
+    }
+}
---
+++ utils/uploadPicToCloudinary.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const uploadPic = async media => {
+  try {
+    const form = new FormData();
+    form.append("file", media);
+    form.append("upload_preset", "socmed");
+    form.append("cloud_name", "faith-l3s5");
+
+    const res = await axios.post(process.env.CLOUDINARY_URL, form);
+    return res.data.url;
+  } catch (error) {
+    return;
+  }
+};
+
+export default uploadPic;
